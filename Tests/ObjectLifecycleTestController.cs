c1692bac690c8025001206b16fbb79c3a79ed0f9
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-29 20:31:52 +0100

    add lifecycle test scene

---
+++ Tests/ObjectLifecycleTestController.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using Object = System.Object;
+
+namespace LunyScript.Unity.Tests
+{
+	[DefaultExecutionOrder(-1000)]
+	public sealed class ObjectLifecycleTestController : MonoBehaviour
+	{
+		[Header("Tests")]
+		public Boolean Assert_Runs_WhenCreated_Passed;
+		public Boolean Assert_Runs_WhenDestroyed_Passed;
+		public Boolean Assert_Runs_WhenEnabled_Passed;
+		public Boolean Assert_Runs_WhenDisabled_Passed;
+		public Boolean Assert_Runs_WhenReady_Passed;
+		public Boolean Assert_Runs_EveryFixedStep_Passed;
+		public Boolean Assert_Runs_EveryFrame_Passed;
+		public Boolean Assert_Runs_EveryFrameEnds_Passed;
+		private IVariables _vars;
+
+		private void Awake()
+		{
+			_vars = LunyScriptEngine.Instance.GlobalVariables;
+			_vars.OnVariableChanged += OnVariableChanged;
+		}
+
+		private void OnDestroy() => _vars.OnVariableChanged -= OnVariableChanged;
+
+		private void OnVariableChanged(Object sender, VariableChangedEventArgs e)
+		{
+			Debug.Log($"Test variable changed: {e}");
+
+			var pass = e.Variable.AsBoolean();
+			if (e.Name == nameof(Assert_Runs_WhenCreated))
+				Assert_Runs_WhenCreated_Passed = pass;
+			else if (e.Name == nameof(Assert_Runs_WhenDestroyed))
+				Assert_Runs_WhenDestroyed_Passed = pass;
+			else if (e.Name == nameof(Assert_Runs_WhenEnabled))
+				Assert_Runs_WhenEnabled_Passed = pass;
+			else if (e.Name == nameof(Assert_Runs_WhenDisabled))
+				Assert_Runs_WhenDisabled_Passed = pass;
+			else if (e.Name == nameof(Assert_Runs_WhenReady))
+				Assert_Runs_WhenReady_Passed = pass;
+			else if (e.Name == nameof(Assert_Runs_EveryFixedStep))
+				Assert_Runs_EveryFixedStep_Passed = pass;
+			else if (e.Name == nameof(Assert_Runs_EveryFrame))
+				Assert_Runs_EveryFrame_Passed = pass;
+			else if (e.Name == nameof(Assert_Runs_EveryFrameEnds))
+				Assert_Runs_EveryFrameEnds_Passed = pass;
+			else
+				throw new ArgumentOutOfRangeException(nameof(e.Name));
+		}
+	}
+}
---
+++ Tests/ObjectLifecycleTests.cs
@@ -0,0 +1,60 @@
+﻿namespace LunyScript.Unity.Tests
+{
+	public abstract class LunyScriptTestBase : LunyScript
+	{
+		protected void DidRun()
+		{
+			var name = GetType().Name;
+			if (!GlobalVariables[name].AsBoolean())
+				GlobalVariables[name] = true;
+		}
+	}
+
+	public sealed class Assert_Runs_WhenCreated : LunyScriptTestBase
+	{
+		public override void Build() => When.Created(Run(DidRun));
+	}
+
+	public sealed class Assert_Runs_WhenDestroyed : LunyScriptTestBase
+	{
+		public override void Build()
+		{
+			When.Created(Object.Destroy());
+			When.Destroyed(Run(DidRun));
+		}
+	}
+
+	public sealed class Assert_Runs_WhenEnabled : LunyScriptTestBase
+	{
+		public override void Build() => When.Enabled(Run(DidRun));
+	}
+
+	public sealed class Assert_Runs_WhenDisabled : LunyScriptTestBase
+	{
+		public override void Build()
+		{
+			When.Created(Object.SetDisabled());
+			When.Disabled(Run(DidRun));
+		}
+	}
+
+	public sealed class Assert_Runs_WhenReady : LunyScriptTestBase
+	{
+		public override void Build() => When.Ready(Run(DidRun));
+	}
+
+	public sealed class Assert_Runs_EveryFixedStep : LunyScriptTestBase
+	{
+		public override void Build() => Every.FixedStep(Run(DidRun));
+	}
+
+	public sealed class Assert_Runs_EveryFrame : LunyScriptTestBase
+	{
+		public override void Build() => Every.Frame(Run(DidRun));
+	}
+
+	public sealed class Assert_Runs_EveryFrameEnds : LunyScriptTestBase
+	{
+		public override void Build() => Every.FrameEnds(Run(DidRun));
+	}
+}
